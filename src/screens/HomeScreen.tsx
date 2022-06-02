f243132c86d08fc04e7b78833e74723a86061561
Sergio Andrés Ruiz Vélez <sergio.ruiz@carroya.com>
2022-06-01 21:34:01 -0500

    Tipado de la petición GET movieDB

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,13 +1,14 @@
 import React, {useEffect} from 'react';
 import {Text, View} from 'react-native';
 import movieDB from '../api/movieDB';
+import {MovieDBNowPlaying} from '../interfaces/movieInterface';
 
 const HomeScreen = () => {
   useEffect(() => {
     movieDB
-      .get('/now_playing')
+      .get<MovieDBNowPlaying>('/now_playing')
       .then(resp => {
-        console.log(resp.data);
+        console.log(resp.data.results[0].title);
       })
       .catch(error => console.error(error));
   }, []);
